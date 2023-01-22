15d73263a5233e32719e02a016ded02cba8b9dd5
zababurinph <raccoontd@mail.ru>
2023-01-23 00:18:23 +0300

    ver1.0

---
+++ script.js
@@ -0,0 +1,15 @@
+const notify = () => {
+    alert("Looking forward to meeting you ^_^")
+}
+
+elusiveButton = document.getElementById("elusive")
+elusiveButton.addEventListener('mouseover', () => {
+    elusiveButton.style.position = "absolute";
+    elusiveButton.style.left = `${random(0, 90)}%`;
+    elusiveButton.style.top = `${random(0, 90)}%`;
+});
+
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+}
